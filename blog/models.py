a2ddc7dcbe6cd7e66a04f58f41dbf1aacf99c054
Vladimir Marushkevich <contact.marushkevich@gmail.com>
2023-04-04 02:39:47 +0300

    added Post, Author and Category models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+User = get_user_model()
 
-# Create your models here.
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+    subtitle = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    thumbnail = models.ImageField()
+    categories = models.ManyToManyField(Category)
+    featured = models.BooleanField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def update_views_counter(self):
+        self.views = self.views + 1
+        super(Post, self).save()
