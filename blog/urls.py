82cf26acb5d0fe14d9de96fdc458049ffff160af
Vladimir Marushkevich <contact.marushkevich@gmail.com>
2023-04-05 15:01:54 +0300

    linked routing in urls.py with views.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('blog/', views.blog_page, name='blog'),
+    path('blog/new-post/', views.add_post, name='new_post'),
+    path('blog/<int:pk>/', views.single_post, name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def home(request):
     return HttpResponse('Hello page')
+
+
+def blog_page(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/blog_page.html', context)
+
+
+def add_post(request):
+    return HttpResponse('New post page')
+
+
+def single_post(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/single_post_page.html', context)
