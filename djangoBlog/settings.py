5b984083db6dffc9b2d20e883c89753702b8ea73
Vladimir Marushkevich <contact.marushkevich@gmail.com>
2023-04-10 13:38:21 +0300

    added static files (collectstatic), setted apps in settings.py

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'blog_auth.apps.BlogAuthConfig',
+    'blog_comments.apps.BlogCommentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / "static_cdn"
+MEDIA_ROOT = BASE_DIR / "media_cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
