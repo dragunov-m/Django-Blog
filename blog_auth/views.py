b56d7de73a5bd73d2768fc02d85299b0f647b1c8
Vladimir Marushkevich <contact.marushkevich@gmail.com>
2023-04-12 17:36:08 +0300

    small fixes in blog_auth

--- blog_auth/views.py
+++ blog_auth/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib import messages
 from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
+User = get_user_model()
 
 
 @login_required(login_url='blog_auth:login')
