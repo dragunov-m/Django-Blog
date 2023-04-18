5a347f8cd53495d8737951de93f9398d9272d4ef
Vladimir Marushkevich <contact.marushkevich@gmail.com>
2023-04-18 12:32:12 +0300

    signup changed from fbv to cbv

--- blog_auth/urls.py
+++ blog_auth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from .views import SignUp
 from . import views
 
 app_name = 'blog_auth'
 urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('logout/', views.logout_page, name='logout'),
-    path('signup/', views.signup_page, name='signup'),
+    path('signup/', SignUp.as_view(), name='signup'),
     path('profile/', views.profile, name='profile'),
 ]
--- blog_auth/views.py
+++ blog_auth/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout, get_user_model
+# from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.contrib import messages
+from django.views.generic import CreateView
 from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
+# from blog.models import Author
 User = get_user_model()
 
+
+# class ProfilePage(LoginRequiredMixin, CreateView):
+#     model = Author
+#     form_class = ProfileUpdateForm
+#     template_name = 'blog_auth/profile_page.html'
+#     success_url = reverse_lazy('blog_auth:profile')
+#     login_url = 'blog_auth:login'
+#
+#     def get_form_kwargs(self):
+#         kwargs = super().get_form_kwargs()
+#         kwargs['user'] = self.request.user
+#         return kwargs
+#
+#     def form_valid(self, form):
+#         u_form = UserUpdateForm(self.request.POST, instance=self.request.user)
+#         p_form = form
+#         if u_form.is_valid() and p_form.is_valid():
+#             u_form.save()
+#             p_form.save()
+#             messages.success(self.request, f'Your profile is updated.')
+#             return super().form_valid(form)
+#         else:
+#             return self.form_invalid(form)
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['u_form'] = UserUpdateForm(instance=self.request.user)
+#         return context
 
 @login_required(login_url='blog_auth:login')
 def profile(request):
@@ -56,15 +88,29 @@ def logout_page(request):
     return redirect('blog:home')
 
 
-def signup_page(request):
-    if request.user.is_authenticated:
-        return redirect('blog:home')
-    else:
-        if request.method == 'POST':
-            form = UserRegistrationForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return redirect('blog:home')
-        else:
-            form = UserRegistrationForm()
-    return render(request, 'blog_auth/signup_page.html', {'form': form})
+class SignUp(CreateView):
+    template_name = 'blog_auth/signup_page.html'
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('blog:home')
+
+    # def get(self, request, *args, **kwargs):
+    #     if self.request.user.is_authenticated:
+    #         return redirect(self.get_success_url())
+    #     else:
+    #         return super().get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+# def signup_page(request):
+#     if request.user.is_authenticated:
+#         return redirect('blog:home')
+#     else:
+#         if request.method == 'POST':
+#             form = UserRegistrationForm(request.POST)
+#             if form.is_valid():
+#                 form.save()
+#                 return redirect('blog:home')
+#         else:
+#             form = UserRegistrationForm()
+#     return render(request, 'blog_auth/signup_page.html', {'form': form})
