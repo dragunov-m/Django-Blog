b7ad10f3e1f3abbe36a857230a1b45d4abe37517
Vladimir Marushkevich <contact.marushkevich@gmail.com>
2023-05-05 11:52:34 +0300

    + comment form and edited model

--- blog_comments/models.py
+++ blog_comments/models.py
@@ -5,24 +5,13 @@ User = get_user_model()
 
 
 class Comment(models.Model):
-    CommentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
-    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
 
     class Meta:
         ordering = ['-pub_date']
 
     def __str__(self):
-        return str(self.author) + 'comment' + str(self.content)
-
-    @property
-    def children(self):
-        return Comment.objects.filter(parent=self).reverse()
-
-    @property
-    def is_parent(self):
-        if self.parent is None:
-            return True
-        return False
+        return f'Comment Author: {self.author} / Post ID: {self.post_id}'
